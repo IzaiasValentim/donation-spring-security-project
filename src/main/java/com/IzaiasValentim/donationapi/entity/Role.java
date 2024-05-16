05d7e0832edb1ca4ec6f153a3d09bb46fa98c3b8
Izaias Valentim <izaias451@gmail.com>
2024-05-16 14:44:19 -0300

    feat/added the annotations for id.

--- src/main/java/com/IzaiasValentim/donationapi/entity/Role.java
+++ src/main/java/com/IzaiasValentim/donationapi/entity/Role.java
@@ -1,13 +1,19 @@
 package com.IzaiasValentim.donationapi.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="ROLES")
 public class Role {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "role_id")
     private Long id;
     private String name;
-    
+
 }
