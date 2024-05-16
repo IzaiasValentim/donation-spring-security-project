3bedd2202d2579a067e38215df9705d28e6b0f90
Izaias Valentim <izaias451@gmail.com>
2024-05-16 16:28:01 -0300

    feat/added the controller for auth.

---
+++ src/main/java/com/IzaiasValentim/donationapi/controller/AuthenticationController.java
@@ -0,0 +1,8 @@
+package com.IzaiasValentim.donationapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    
+}
