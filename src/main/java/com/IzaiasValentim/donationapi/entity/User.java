96e51034f3d8929716148c1698badb5049723e32
Izaias Valentim <izaias451@gmail.com>
2024-05-16 14:32:52 -0300

    feat/defined the structure of a user.

--- src/main/java/com/IzaiasValentim/donationapi/entity/User.java
+++ src/main/java/com/IzaiasValentim/donationapi/entity/User.java
@@ -1,5 +1,18 @@
 package com.IzaiasValentim.donationapi.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="USERS")
 public class User {
-    
+
+    private Long id;
+    private String username;
+    private String password;
+    private Set<Role> roles = new HashSet<>();
+
 }
