593e1af596b48f43261cf2ae47ab3e7285ce5dac
Izaias Valentim <izaias451@gmail.com>
2024-05-16 14:38:24 -0300

    feat/added some annotations for the control of Id; Constructors and Getters and Setters.

--- src/main/java/com/IzaiasValentim/donationapi/entity/User.java
+++ src/main/java/com/IzaiasValentim/donationapi/entity/User.java
@@ -3,16 +3,63 @@ package com.IzaiasValentim.donationapi.entity;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="USERS")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
     private Long id;
     private String username;
     private String password;
+
     private Set<Role> roles = new HashSet<>();
 
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+    
 }
