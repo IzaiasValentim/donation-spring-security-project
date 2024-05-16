6cd9a611973c803d520239c27e16b9134d71fd65
Izaias Valentim <izaias451@gmail.com>
2024-05-16 16:33:38 -0300

    feat/added a route to authenticate.

--- src/main/java/com/IzaiasValentim/donationapi/controller/AuthenticationController.java
+++ src/main/java/com/IzaiasValentim/donationapi/controller/AuthenticationController.java
@@ -1,8 +1,21 @@
 package com.IzaiasValentim.donationapi.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.IzaiasValentim.donationapi.service.AuthenticationService;
 
 @RestController
 public class AuthenticationController {
-    
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("authenticate")
+    public String authenticate(Authentication authentication) {
+        return authenticationService.authenticate(authentication);
+    }    
 }
