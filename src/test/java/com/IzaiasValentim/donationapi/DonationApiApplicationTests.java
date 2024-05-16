ef931267c84796ed3748a92832979cb12019228e
Izaias Valentim <izaias451@gmail.com>
2024-05-16 14:14:11 -0300

    feat/added the base project and changed the .gitignore.

---
+++ src/test/java/com/IzaiasValentim/donationapi/DonationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IzaiasValentim.donationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
