684c067d9dc9c7e907a1bbc7d65c394a398bccd3
Izaias Valentim <izaias451@gmail.com>
2024-05-16 14:54:31 -0300

    feat/created the relationship between a user and their roles. An intermediate table was also created to handle this relationship using the @JoinTable annotation.

--- src/main/java/com/IzaiasValentim/donationapi/entity/User.java
+++ src/main/java/com/IzaiasValentim/donationapi/entity/User.java
@@ -3,23 +3,30 @@ package com.IzaiasValentim.donationapi.entity;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="USERS")
+@Table(name = "USERS")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="user_id")
+    @Column(name = "user_id")
     private Long id;
     private String username;
     private String password;
 
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public User() {
@@ -59,7 +66,6 @@ public class User {
     }
 
     public void setRoles(Set<Role> roles) {
-        this.roles = roles;
+        this.roles.addAll(roles);
     }
-    
 }
