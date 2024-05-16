c6463c002cd4eee77d2aceb088852baf06f08941
Izaias Valentim <izaias451@gmail.com>
2024-05-16 15:07:37 -0300

    feat/created the file for the controller layer of user functions. Initially there will be only one test method.

---
+++ src/main/java/com/IzaiasValentim/donationapi/controller/userController.java
@@ -0,0 +1,17 @@
+package com.IzaiasValentim.donationapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping(value="managers")
+public class userController {
+
+    @GetMapping("/say-hello")
+    public String securedHelloWorld() {
+        return "Hello, secure world!";
+    }
+    
+}
