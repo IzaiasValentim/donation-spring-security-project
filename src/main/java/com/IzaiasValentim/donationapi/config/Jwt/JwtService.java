5e33f0cb472c3069fa8f120f90f7c2e41641ea23
Izaias Valentim <izaias451@gmail.com>
2024-05-16 16:25:37 -0300

    feat/create the JwtService. The methos generate Token return a token if a user is successfully authenticated.

--- src/main/java/com/IzaiasValentim/donationapi/config/Jwt/JwtService.java
+++ src/main/java/com/IzaiasValentim/donationapi/config/Jwt/JwtService.java
@@ -1,8 +1,40 @@
 package com.IzaiasValentim.donationapi.config.Jwt;
 
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
-    
+    private final JwtEncoder jwtEncoder;
+
+    @Autowired
+    public JwtService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    public String generateToken(Authentication authentication) {
+        Instant now = Instant.now();
+        long expiry = 3600L;
+
+        String scopes = authentication.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        var claims = JwtClaimsSet.builder()
+                .issuer("donation-api")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiry))
+                .subject(authentication.getName())
+                .claim("scope", scopes)
+                .build();
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
 }
