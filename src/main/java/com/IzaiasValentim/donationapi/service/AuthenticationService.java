3dc1953b3b4a0f78dd2ac082b09cab80a88dc310
Izaias Valentim <izaias451@gmail.com>
2024-05-16 16:27:33 -0300

    feat/created the method of authentication.

--- src/main/java/com/IzaiasValentim/donationapi/service/AuthenticationService.java
+++ src/main/java/com/IzaiasValentim/donationapi/service/AuthenticationService.java
@@ -1,9 +1,19 @@
 package com.IzaiasValentim.donationapi.service;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-
+import com.IzaiasValentim.donationapi.config.Jwt.JwtService;
 
 @Service
 public class AuthenticationService {
+    private final JwtService jwtService;
+
+    public AuthenticationService(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    public String authenticate(Authentication authentication){
+        return jwtService.generateToken(authentication);
+    }
 }
