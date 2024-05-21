b2b97d01d4c85ee427cb64c420af0a2dcf426c36
Izaias Valentim <izaias451@gmail.com>
2024-05-21 15:22:03 -0300

    feat/ Creation of Donation Entity.

---
+++ src/main/java/com/IzaiasValentim/donationapi/entity/Donation.java
@@ -0,0 +1,26 @@
+package com.IzaiasValentim.donationapi.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "DONATIONS")
+public class Donation {
+    private Long id;
+    private String donor_Name;
+    private String phone;
+    private String email;
+    private Double value;
+    private String transferVoucher;
+    private Boolean anonymous;
+    private Boolean verified;
+    private LocalDateTime created;
+    private LocalDateTime validationTime;
+    private User userOfValidation;
+}
