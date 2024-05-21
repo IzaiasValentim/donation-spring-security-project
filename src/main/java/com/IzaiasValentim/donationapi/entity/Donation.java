9a92ca19c2051c4dd04090ababb81110c7f5e78e
Izaias Valentim <izaias451@gmail.com>
2024-05-21 15:28:19 -0300

    feat/ Constructor and Getters and setters.

--- src/main/java/com/IzaiasValentim/donationapi/entity/Donation.java
+++ src/main/java/com/IzaiasValentim/donationapi/entity/Donation.java
@@ -2,6 +2,8 @@ package com.IzaiasValentim.donationapi.entity;
 
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -25,8 +27,101 @@ public class Donation {
     private String transferVoucher;
     private Boolean anonymous;
     private Boolean verified;
+    @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime created;
     private LocalDateTime validationTime;
     private User userOfValidation;
+
+    public Donation() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDonor_Name() {
+        return donor_Name;
+    }
+
+    public void setDonor_Name(String donor_Name) {
+        this.donor_Name = donor_Name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getTransferVoucher() {
+        return transferVoucher;
+    }
+
+    public void setTransferVoucher(String transferVoucher) {
+        this.transferVoucher = transferVoucher;
+    }
+
+    public Boolean getAnonymous() {
+        return anonymous;
+    }
+
+    public void setAnonymous(Boolean anonymous) {
+        this.anonymous = anonymous;
+    }
+
+    public Boolean getVerified() {
+        return verified;
+    }
+
+    public void setVerified(Boolean verified) {
+        this.verified = verified;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getValidationTime() {
+        return validationTime;
+    }
+
+    public void setValidationTime(LocalDateTime validationTime) {
+        this.validationTime = validationTime;
+    }
+
+    public User getUserOfValidation() {
+        return userOfValidation;
+    }
+
+    public void setUserOfValidation(User userOfValidation) {
+        this.userOfValidation = userOfValidation;
+    }
 
 }
