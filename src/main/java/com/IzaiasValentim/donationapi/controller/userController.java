6b37aa75c663a0fd0fd2e0bc19ea84187b0d8715
Izaias Valentim <izaias451@gmail.com>
2024-05-21 14:20:54 -0300

    feat/Another route was added to the managers' resources and defined via the @PreAuthorize annotation which role will have access to each route.

--- src/main/java/com/IzaiasValentim/donationapi/controller/userController.java
+++ src/main/java/com/IzaiasValentim/donationapi/controller/userController.java
@@ -2,6 +2,7 @@ package com.IzaiasValentim.donationapi.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -9,9 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping(value="managers")
 public class UserController {
 
-    @GetMapping("/say-hello")
+    @GetMapping("/say-hello-total")
+    @PreAuthorize("hasAuthority('SCOPE_TOTAL') && hasAuthority('SCOPE_PARTIAL')")
     public String securedHelloWorld() {
         return "Hello, secure world!";
     }
-    
+    @GetMapping("/say-hello-partial")
+    @PreAuthorize("hasAuthority('SCOPE_TOTAL') || hasAuthority('SCOPE_PARTIAL')")
+    public String securedHelloWorldPartial() {
+        return "Hello, secure world!";
+    }
 }
