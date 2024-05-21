4a07a5e6a99c7e71d372543c07d33a867a29de2c
Izaias Valentim <izaias451@gmail.com>
2024-05-21 15:31:04 -0300

    feat/ Added the annotation for Repository.

--- src/main/java/com/IzaiasValentim/donationapi/repository/DonationRepository.java
+++ src/main/java/com/IzaiasValentim/donationapi/repository/DonationRepository.java
@@ -1,9 +1,11 @@
 package com.IzaiasValentim.donationapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.IzaiasValentim.donationapi.entity.Donation;
 
+@Repository
 public interface DonationRepository extends JpaRepository<Donation, Long>{
     
 }
