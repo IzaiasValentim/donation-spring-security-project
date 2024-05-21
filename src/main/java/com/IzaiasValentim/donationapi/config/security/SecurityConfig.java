a1e1e25925aa79a124d6abb9dd31b7001593975a
Izaias Valentim <izaias451@gmail.com>
2024-05-21 14:12:33 -0300

    feat/ read th private and public keys.

--- src/main/java/com/IzaiasValentim/donationapi/config/security/SecurityConfig.java
+++ src/main/java/com/IzaiasValentim/donationapi/config/security/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.IzaiasValentim.donationapi.config.security;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,4 +12,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKet;
+
 }
