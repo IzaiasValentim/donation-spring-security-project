4ea9f6d06bbc06677f6630c2b995a424f3f97085
Izaias Valentim <izaias451@gmail.com>
2024-05-21 15:38:07 -0300

    feature/ Createdd the first method to returns all donations on database.

--- src/main/java/com/IzaiasValentim/donationapi/service/DonationService.java
+++ src/main/java/com/IzaiasValentim/donationapi/service/DonationService.java
@@ -1,5 +1,23 @@
 package com.IzaiasValentim.donationapi.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.IzaiasValentim.donationapi.entity.Donation;
+import com.IzaiasValentim.donationapi.repository.DonationRepository;
+
+@Service
 public class DonationService {
     
+    private final DonationRepository donationRepository;
+
+    @Autowired
+    public DonationService(DonationRepository donationRepository){
+        this.donationRepository = donationRepository;
+    }
+    public List<Donation> getAllDonations(){
+        return donationRepository.findAll();
+    }
 }
