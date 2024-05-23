0053f7572466ccea4e440e0de91f673784879dcf
Izaias Valentim <izaias451@gmail.com>
2024-05-23 16:31:55 -0300

    feat/ Defined the class fields.

---
+++ src/main/java/com/IzaiasValentim/donationapi/entity/dto/DonationCreationDTO.java
@@ -0,0 +1,10 @@
+package com.IzaiasValentim.donationapi.entity.dto;
+
+public class DonationCreationDTO {
+    private String donor_Name;
+    private String phone;
+    private String email;
+    private Double amountDonated;
+    private Boolean anonymous;
+    private String transferVoucher;
+}
