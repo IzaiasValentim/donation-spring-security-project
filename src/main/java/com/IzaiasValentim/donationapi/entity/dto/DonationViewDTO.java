b8c5b5c39abfdc96aae431a6dfe66fbe90b7c848
Izaias Valentim <izaias451@gmail.com>
2024-05-23 16:45:58 -0300

    feat/ Defined the base fields.

--- src/main/java/com/IzaiasValentim/donationapi/entity/dto/DonationViewDTO.java
+++ src/main/java/com/IzaiasValentim/donationapi/entity/dto/DonationViewDTO.java
@@ -1,5 +1,9 @@
 package com.IzaiasValentim.donationapi.entity.dto;
 
+import java.time.LocalDateTime;
+
 public class DonationViewDTO {
-    
+    private Double amountDonated;
+    private Boolean anonymous;
+    private LocalDateTime created;
 }
