68bc4bcd79ca9a9d07679dd47123b6abfe302aad
Izaias Valentim <izaias451@gmail.com>
2024-05-24 11:03:43 -0300

    feat/ Create a method to save one Donation.

--- src/main/java/com/IzaiasValentim/donationapi/service/DonationService.java
+++ src/main/java/com/IzaiasValentim/donationapi/service/DonationService.java
@@ -1,14 +1,20 @@
 package com.IzaiasValentim.donationapi.service;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.IzaiasValentim.donationapi.entity.Donation;
+import com.IzaiasValentim.donationapi.entity.dto.DonationCreationDTO;
 import com.IzaiasValentim.donationapi.entity.dto.DonationViewDTO;
 import com.IzaiasValentim.donationapi.repository.DonationRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class DonationService {
@@ -32,5 +38,22 @@ public class DonationService {
         });
 
         return donationsViewDTO;
+    }
+
+    @Transactional
+    public ResponseEntity<DonationViewDTO> saveDonation(DonationCreationDTO donation){
+        Donation donationToSave = donation.dtoToDonation();
+        donationToSave.setValidationTime(null);
+        donationToSave.setVerificationUserUsername("Not yet validated.");
+        donationRepository.save(donationToSave);
+        
+        DonationViewDTO donationReturned   = new DonationViewDTO(donationToSave);
+
+        URI readerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("id={id}")
+                .buildAndExpand(donationToSave.getId())
+                .toUri();
+        return ResponseEntity.created(readerLocation).body(donationReturned);
     }
 }
