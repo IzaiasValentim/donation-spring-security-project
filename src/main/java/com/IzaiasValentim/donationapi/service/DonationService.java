8ec86678a9f9ab8ae02a7e84b234b2a9a4affed6
Izaias Valentim <izaias451@gmail.com>
2024-05-24 10:34:37 -0300

    feat/ The method that returns all registered donations was implemented. The return is done as DTO to protect some data information. The required transparency is the consultation of donation amounts and dates.

--- src/main/java/com/IzaiasValentim/donationapi/service/DonationService.java
+++ src/main/java/com/IzaiasValentim/donationapi/service/DonationService.java
@@ -1,23 +1,36 @@
 package com.IzaiasValentim.donationapi.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.IzaiasValentim.donationapi.entity.Donation;
+import com.IzaiasValentim.donationapi.entity.dto.DonationViewDTO;
 import com.IzaiasValentim.donationapi.repository.DonationRepository;
 
 @Service
 public class DonationService {
-    
+
     private final DonationRepository donationRepository;
 
     @Autowired
-    public DonationService(DonationRepository donationRepository){
+    public DonationService(DonationRepository donationRepository) {
         this.donationRepository = donationRepository;
     }
-    public List<Donation> getAllDonations(){
+
+    public List<Donation> getAllDonations() {
         return donationRepository.findAll();
+    }
+
+    public List<DonationViewDTO> getAllDonationPublicView() {
+        List<DonationViewDTO> donationsViewDTO = new ArrayList<>();
+
+        donationRepository.findAll().forEach(donation -> {
+            donationsViewDTO.add(new DonationViewDTO(donation));
+        });
+
+        return donationsViewDTO;
     }
 }
