438e1dfedf44a5fceda986db68cc8c396c3b5c44
Izaias Valentim <izaias451@gmail.com>
2024-05-24 14:24:09 -0300

    feat/ I created a route to perform an action. The donation route is public. I also added a route for managers that returns more information about donations.

--- src/main/java/com/IzaiasValentim/donationapi/controller/DonationController.java
+++ src/main/java/com/IzaiasValentim/donationapi/controller/DonationController.java
@@ -3,15 +3,20 @@ package com.IzaiasValentim.donationapi.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.service.annotation.GetExchange;
 
 import com.IzaiasValentim.donationapi.entity.Donation;
+import com.IzaiasValentim.donationapi.entity.dto.DonationCreationDTO;
 import com.IzaiasValentim.donationapi.entity.dto.DonationViewDTO;
 import com.IzaiasValentim.donationapi.service.DonationService;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -24,9 +29,25 @@ public class DonationController {
         this.donationService = donationService;
     }
 
+    @GetMapping("/viewAll")
+    @PreAuthorize("hasAuthority('SCOPE_TOTAL') || hasAuthority('SCOPE_PARTIAL')")
+    public ResponseEntity<List<Donation>> getAllDonationsManagerView() {
+        return ResponseEntity.ok().body(donationService.getAllDonations());
+    }
+
     @GetMapping
     public ResponseEntity<List<DonationViewDTO>> getAllDonationsPublicView() {
         return ResponseEntity.ok().body(donationService.getAllDonationPublicView());
+    }
+    
+    @PostMapping("/donate/")
+    public ResponseEntity<DonationViewDTO> postMethodName(@RequestBody DonationCreationDTO donationToSave) {
+        if(donationToSave.isNotBlank()){
+            return donationService.saveDonation(donationToSave);
+        }else{
+            return ResponseEntity.badRequest().build();
+        }
+            
     }
     
 
